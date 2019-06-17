f66e1c844f4cf0c7f7ee19862fc3dec42ed313f2
Carlos Sandoval <iznomag@gmail.com>
2019-06-17 17:17:54 -0500

    Add more dispatc action

--- store/reducer.js
+++ store/reducer.js
@@ -6,9 +6,16 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
       return { counter: state.counter + 1 };
+    case "DECREMENT":
+      return { counter: state.counter - 1 };
+    case "ADD":
+      return { counter: state.counter + action.payload.value };
+    case "SUBTRACT":
+      return { counter: state.counter - action.payload.value };
+    default: {
+      return state;
+    }
   }
-
-  return state;
 };
 
 export default reducer;
